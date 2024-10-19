349c40076248a4c34aeeee3525f44992ceaed218
snomiao <snomiao@gmail.com>
2024-10-19 20:43:54 +0000

    fix(app): type
    type

--- app/pg.ts
+++ app/pg.ts
@@ -1,6 +1,5 @@
-import { varchar } from "drizzle-orm/mysql-core";
 import { drizzle } from "drizzle-orm/neon-http";
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import DIE from "phpdie";
 export const pg = drizzle(process.env.DATABASE_URL ?? DIE(""));
 export const usersTable = pgTable("users", {
