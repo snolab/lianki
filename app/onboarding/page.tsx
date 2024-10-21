f68279e2b7b4a1abb1070d914119490637e18f35
snomiao <snomiao@gmail.com>
2024-10-21 05:55:33 +0000

    fix(app): no edge
    no edge

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+// 1. create password
+// 2. link oauth
+export default function OnboardingPage(){
+    // 
+    // /onborarding/step1-create-password
+    // /onborarding/step2-link-oauth
+    // /onborarding/step3-tutorial
+    return <></>
+}
---
+++ app/waitlist/page.tsx
@@ -0,0 +1,6 @@
+// waitlist
+// 1. create password
+// 2. link oauth
+export default function WaitlistPage(){
+    return <></>
+}
