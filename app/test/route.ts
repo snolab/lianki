beaefe159e3fcc98ee3a5a08aa83007511e70d2d
snomiao <snomiao@gmail.com>
2024-10-21 06:45:45 +0000

    fix(test): edge db
    edge db

--- app/test/route.ts
+++ app/test/route.ts
@@ -1,5 +1,6 @@
 import { db } from "../db-edge";
 // export const runtime = "edge";
+
 export const GET = async () => {
   return new Response(String(await db.collection("test").countDocuments({})));
 };
