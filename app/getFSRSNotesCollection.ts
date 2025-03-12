9f7e428d4dec64af730a058e2e90b6a98b48e799
snomiao <snomiao@gmail.com>
2025-03-12 01:55:51 +0000

    fix(main): pgpool
    pgpool

--- app/getFSRSNotesCollection.ts
+++ app/getFSRSNotesCollection.ts
@@ -2,7 +2,5 @@ import { db } from "./db";
 import { FSRSNote } from "./fsrs";
 
 export function getFSRSNotesCollection(email: string | undefined) {
-  return db.collection<FSRSNote>(
-    "FSRSNotes" + (email?.replace(/^/, "@") ?? "")
-  );
+  return db.collection<FSRSNote>(`FSRSNotes${email?.replace(/^/, "@") ?? ""}`);
 }
