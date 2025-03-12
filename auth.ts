a8a9c689d1ca7cb5e727ee23b342f2123c15017b
snomiao <snomiao@gmail.com>
2025-03-12 01:53:28 +0000

    fix(main): pgpool
    pgpool

--- auth.ts
+++ auth.ts
@@ -1,48 +1,53 @@
 // import { DrizzleAdapter } from "@auth/drizzle-adapter";
 // import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "@neondatabase/serverless";
+
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
-import { db } from "./schema";
 declare module "next-auth" {
   interface User {
     password?: string;
   }
 }
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: MongoDBAdapter(mongoClient),
-  adapter: DrizzleAdapter(db),
-  // pages: {
-  //   // signIn: '/auth/signin',
-  //   // signOut: '/auth/signout',
-  //   // error: '/auth/error',
-  //   // verifyRequest: '/auth/verify-request',
-  //   // newUser: '/auth/new-user'
-  // },
-  // callbacks: {
-  //   jwt: async ({ token, user }) => {
-  //     if (user) {
-  //       token.id = user.id;
-  //     }
-  //     return token;
-  //   },
-  // },
-  callbacks: {
-    jwt({ token, user }) {
-      if (user) {
-        // User is available during sign-in
-        token.id = user.id;
-      }
-      return token;
-    },
-    session({ session, token }) {
-      // @ts-expect-error any
-      session.user.id = token.id;
-      return session;
+export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return {
+    // adapter: MongoDBAdapter(mongoClient),
+    // adapter: DrizzleAdapter(db),
+    adapter: PostgresAdapter(pool),
+    // pages: {
+    //   // signIn: '/auth/signin',
+    //   // signOut: '/auth/signout',
+    //   // error: '/auth/error',
+    //   // verifyRequest: '/auth/verify-request',
+    //   // newUser: '/auth/new-user'
+    // },
+    // callbacks: {
+    //   jwt: async ({ token, user }) => {
+    //     if (user) {
+    //       token.id = user.id;
+    //     }
+    //     return token;
+    //   },
+    // },
+    callbacks: {
+      jwt({ token, user }) {
+        if (user) {
+          // User is available during sign-in
+          token.id = user.id;
+        }
+        return token;
+      },
+      session({ session, token }) {
+        // @ts-expect-error any
+        session.user.id = token.id;
+        return session;
+      },
     },
 
-  },
-  
-  session: { strategy: "jwt" }, // force JWT session with a database
-  ...authConfig,
+    session: { strategy: "jwt" },
+    // force JWT session with a database
+    ...authConfig,
+  };
 });
